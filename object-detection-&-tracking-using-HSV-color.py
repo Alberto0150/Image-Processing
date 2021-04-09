0e30e29f1e06efd670276d873552f0a6668ebad2
Alberto0150 <albertosanjaya2@gmail.com>
2021-04-09 19:52:07 +0700

    Update object-detection-&-tracking-using-HSV-color.py
    Adding Trackbar to easier changing color set

--- object-detection-&-tracking-using-HSV-color.py
+++ object-detection-&-tracking-using-HSV-color.py
@@ -4,15 +4,36 @@ import numpy as np
 def nothing(x):
     pass
 
+#Creating UI for trackbar
+cv2.namedWindow("Tracking")
+#creating the trackbar
+cv2.createTrackbar("LH", "Tracking", 0, 255, nothing)
+cv2.createTrackbar("LS", "Tracking", 0, 255, nothing)
+cv2.createTrackbar("LV", "Tracking", 0, 255, nothing)
+
+cv2.createTrackbar("UH", "Tracking", 255, 255, nothing)
+cv2.createTrackbar("US", "Tracking", 255, 255, nothing)
+cv2.createTrackbar("UV", "Tracking", 255, 255, nothing)
+
+
 while True:
     frame = cv2.imread('dance.jpg')
 
     #Changing image into HSV color
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
+    #Get trackbar position
+    lowerHue= cv2.getTrackbarPos("LH", "Tracking")
+    lowerSaturation= cv2.getTrackbarPos("LS", "Tracking")
+    lowerValue= cv2.getTrackbarPos("LV", "Tracking")
+
+    upperHue= cv2.getTrackbarPos("UH", "Tracking")
+    upperSaturation= cv2.getTrackbarPos("US", "Tracking")
+    upperValue= cv2.getTrackbarPos("UV", "Tracking")
+
     #Set the upper and lower value
-    getLowerValueBlue = np.array([110, 50, 50])
-    getUpperValueBlue = np.array([130, 255, 255])
+    getLowerValueBlue = np.array([lowerHue, lowerSaturation, lowerValue])
+    getUpperValueBlue = np.array([upperHue, upperSaturation, upperValue])
 
     #set masking so only get the color between upper and lower value
     masking = cv2.inRange(hsv, getLowerValueBlue, getUpperValueBlue)
