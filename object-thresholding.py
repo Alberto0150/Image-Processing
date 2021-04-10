68cc026ecac5e2b175b9c99691acbabb7b946f10
Alberto0150 <albertosanjaya2@gmail.com>
2021-04-10 17:57:49 +0700

    Adding adaptive type thresholding

--- object-thresholding.py
+++ object-thresholding.py
@@ -15,11 +15,20 @@ _, th2 = cv.threshold(img, 127, 255, cv.THRESH_TRUNC)
 #the rest will be the same as default color
 _, th3 = cv.threshold(img, 127, 255, cv.THRESH_TOZERO)
 
+# ADAPTIVE THRESHOLD
+# 2 Type: .ADAPTIVE_THRESH_MEAN_C & .ADAPTIVE_THRESH_GAUSSIAN_C
+# 255 is the maximum value to a pixel
+# 11 is the block size, block size is the size of the neightbourhood area
+# 2 is the C const value
+th4 = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2)
+
 
 cv.imshow('Image', img)
 cv.imshow('th1', th1)
 cv.imshow('th2', th2)
 cv.imshow('th3', th3)
+cv.imshow('th4', th4)
+
 
 
 cv.waitKey(0)
