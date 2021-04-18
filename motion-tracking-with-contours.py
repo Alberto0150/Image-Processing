5deb91bf575f716e0dee1417b67b702b8b71cb62
Alberto0150 <albertosanjaya2@gmail.com>
2021-04-18 17:06:41 +0700

    Update motion-tracking-with-contours.py
    Adding rectangle contour
    Fixing noise with rectangle contour

--- motion-tracking-with-contours.py
+++ motion-tracking-with-contours.py
@@ -10,7 +10,7 @@ while capture.isOpened():
     diff = cv2.absdiff(frame1, frame2)
     # easier to find out contour in the latest stages
     gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
-    #blur the greyscale frame
+    # blur the greyscale frame
     # (5,5) is the kernal size
     blur = cv2.GaussianBlur(gray, (5,5), 0)
     # find out the threshold
@@ -23,11 +23,31 @@ while capture.isOpened():
     # THIRD ARGUMENT = METHOD
     contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    #apply the contours to original frame
+    # rectangle contours
+    for contour in contours:
+        # save coordinate of the contour
+        (x, y, w, h) = cv2.boundingRect(contour)
+
+        # find out the area of the contour, if less than certain value
+        # left the area alone, otherwise draw rectangle
+        if cv2.contourArea(contour) < 900 :
+            continue
+        
+        # draw rectangle with method
+        # SECOND ARGUMENT IS POINT1 
+        # THIRD ARGUMENT IS POINT2
+        # FORTH ARGUMENT IS COLOR
+        # FIFTH ARGUMENT IS THICKNESS
+        cv2.rectangle(frame1, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+
+
+    # outline object contours
+    # apply the contours to original frame
     # THIRD ARGUMENT = CONTOUR ID -> -1 = apply all the contours
     # FORTH ARGUMENT = COLOR
     # FIFTH ARGUMENT = THICKNESS
-    cv2.drawContours(frame1, contours, -1, (0,255,0), 2)
+    #cv2.drawContours(frame1, contours, -1, (0,255,0), 2)
 
 
     cv2.imshow("feed", frame1)
