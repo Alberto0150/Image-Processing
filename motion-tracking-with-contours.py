da94a4d051779bdd5bc3a89d112c459bf7e82238
Alberto0150 <albertosanjaya2@gmail.com>
2021-04-18 16:47:42 +0700

    Update motion-tracking-with-contours.py
    Done creating contours
    (-) have noise
    
    Contour pattern following the object

--- motion-tracking-with-contours.py
+++ motion-tracking-with-contours.py
@@ -10,8 +10,33 @@ while capture.isOpened():
     diff = cv2.absdiff(frame1, frame2)
     # easier to find out contour in the latest stages
     gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+    #blur the greyscale frame
+    # (5,5) is the kernal size
+    blur = cv2.GaussianBlur(gray, (5,5), 0)
+    # find out the threshold
+    _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
 
-    cv2.imshow("inter", frame1)
+    dilated = cv2.dilate(thresh, None, iterations=3)
+
+    # find out the contour
+    # SECOND ARGUMENT = MODE
+    # THIRD ARGUMENT = METHOD
+    contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    #apply the contours to original frame
+    # THIRD ARGUMENT = CONTOUR ID -> -1 = apply all the contours
+    # FORTH ARGUMENT = COLOR
+    # FIFTH ARGUMENT = THICKNESS
+    cv2.drawContours(frame1, contours, -1, (0,255,0), 2)
+
+
+    cv2.imshow("feed", frame1)
+
+    # assign value frame1 from frame2
+    frame1=frame2
+
+    #reading new frame in frame2
+    ret, frame2 = capture.read()
 
     if cv2.waitKey(40)==27:
         break
